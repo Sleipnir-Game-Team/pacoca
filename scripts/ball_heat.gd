caa57eedfaa4ab04d70d468cc27c809628aed6cf
Erik Felipe Viana Rocha <erikviana32@gmail.com>
2024-10-19 17:27:42 -0400

    0.1.7.1 [F] - heat system and start of enemies intelligence

---
+++ scripts/ball_heat.gd
@@ -0,0 +1,24 @@
+extends Node
+
+@export var heat := 0
+@export var speed := 0
+var add_speed
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func increment_heat():
+	heat += 1
+	
+func speed_up():
+	add_speed = heat * speed
+
+func decresse_heat():
+	if heat > 0:
+		heat -= 1
