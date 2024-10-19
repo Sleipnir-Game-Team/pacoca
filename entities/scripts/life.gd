b9ae3b01977b5ef86c5b0ccb95c64a93917397a2
FMABr <felipemuamorim@gmail.com>
2024-10-19 12:05:59 -0400

    0.1.3.3 - Move life script

--- entities/scripts/life.gd
+++ entities/scripts/life.gd
@@ -7,18 +7,13 @@ var entity_life
 func _ready() -> void:
 	entity_life = max_life
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
 func damage():
 	if entity_life > 0:
 		entity_life -= 1
 		
 	if entity_life == 0:
 		defeat()
-	
+
 func recover():
 	if entity_life < max_life and entity_life > 0:
 		entity_life += 1
