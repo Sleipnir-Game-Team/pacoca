f48b074e1a5dc3a4ff4c3b5ee52840dd55993a43
luiz <lv3to.sunking@gmail.com>
2024-10-18 20:18:46 -0400

    Movido os assets pra pasta de assets pq eu lesei

--- addons/SleipnirAudio/plugincore.gd
+++ addons/SleipnirAudio/plugincore.gd
@@ -7,25 +7,25 @@ const SFXGLOBALS_SINGLETON   = "SfxGlobals"
 
 func _enter_tree() -> void:
 	add_custom_type("SoundQueue", "Node",
-	preload("SFX/SoundQueue.gd"),preload("Icons/soundqueue.png"))
+	preload("SFX/SoundQueue.gd"),preload("res://assets/Icons/soundqueue.png"))
 	
 	add_custom_type("SoundQueue2D", "Node2D",
-	preload("SFX/SoundQueue2D.gd"),preload("Icons/soundqueue2d.png"))
+	preload("SFX/SoundQueue2D.gd"),preload("res://assets/Icons/soundqueue2d.png"))
 	
 	add_autoload_singleton(SFXGLOBALS_SINGLETON,"SFX/SFXGlobals.tscn")
 	add_autoload_singleton(AUDIOMANAGER_SINGLETON,"Manager/AudioManager.gd")
 		
 	add_custom_type("SongData","Resource",
-	preload("Music/MaestroCore/SongData.gd"),preload("Icons/song_data.png"))
+	preload("Music/MaestroCore/SongData.gd"),preload("res://assets/Icons/song_data.png"))
 
 	add_custom_type("AudioInteractivePlayer","AudioStreamPlayer",
-	preload("Music/MaestroCore/AudioInteractivePlayer.gd"),preload("Icons/AudioPlayerInteractive.svg"))
+	preload("Music/MaestroCore/AudioInteractivePlayer.gd"),preload("res://assets/Icons/AudioPlayerInteractive.svg"))
 
 	add_custom_type("AudioSyncPlayer","AudioStreamPlayer",
-	preload("Music/MaestroCore/AudioSyncPlayer.gd"),preload("Icons/AudioPlayerInteractive.svg"))
+	preload("Music/MaestroCore/AudioSyncPlayer.gd"),preload("res://assets/Icons/AudioPlayerInteractive.svg"))
 	
 	add_custom_type("AudioListPlayer","AudioStreamPlayer",
-	preload("Music/MaestroCore/AudioListPlayer.gd"),preload("Icons/AudioPlayerList.svg"))
+	preload("Music/MaestroCore/AudioListPlayer.gd"),preload("res://assets/Icons/AudioPlayerList.svg"))
 	
 	add_autoload_singleton(MAESTRO_SINGLETON,"Music/SleipnirMaestro.tscn")
 
