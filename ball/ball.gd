3edc271e2b4c7f749639b159d3d716c75aab31d0
Erik Felipe Viana Rocha <erikviana32@gmail.com>
2024-10-19 18:56:38 -0400

    0.1.11.1 - Heat system

--- ball/ball.gd
+++ ball/ball.gd
@@ -7,13 +7,14 @@ var direction: float = 30
 var bounce: Vector2
 
 @onready var contact_area: ContactArea = $ContactArea
+@onready var heat := $Heat as Node2D
 
 func _ready() -> void:
-	velocity = Vector2(sin(deg_to_rad(direction)) * speed, cos(deg_to_rad(direction)) * speed)
+	velocity = Vector2(sin(deg_to_rad(direction)) * speed * heat.add_speed, cos(deg_to_rad(direction)) * speed * heat.add_speed)
 
 func change_angle(angle: float) -> void:
 	direction = angle
-	velocity = Vector2(cos(deg_to_rad(direction)) * speed, sin(deg_to_rad(direction)) * speed)
+	velocity = Vector2(cos(deg_to_rad(direction)) * speed * heat.add_speed, sin(deg_to_rad(direction)) * speed * heat.add_speed)
 
 func flip(normal: Vector2) -> void:
 	velocity = velocity.bounce(normal)
@@ -21,3 +22,7 @@ func flip(normal: Vector2) -> void:
 
 func _physics_process(_delta: float) -> void:
 	move_and_slide()
+	
+func add_heat():
+	heat.increment_heat()
+	print(heat.heat)
--- scripts/ball_heat.gd
+++ scripts/ball_heat.gd
@@ -6,8 +6,7 @@ var add_speed
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	add_speed = speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
