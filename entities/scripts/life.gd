c717e15ea8410baff7d6d74e5fe278f727060a46
FMABr <felipemuamorim@gmail.com>
2024-10-19 12:23:30 -0400

    0.1.3.7 - Finish life component

--- entities/scripts/life.gd
+++ entities/scripts/life.gd
@@ -8,16 +8,13 @@ signal defeat_signal
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	entity_life = max_life
-	
-func _process(delta: float) -> void:
-	defeat_signal.connect(defeat)
 
 func damage():
 	if entity_life > 0:
 		entity_life -= 1
 		
 	if entity_life == 0:
-		emit_signal("defeat_signal")
+		defeat_signal.emit()
 
 func recover():
 	if entity_life < max_life and entity_life > 0:
@@ -26,6 +23,3 @@ func recover():
 		print("vida cheia")
 	elif entity_life <= 0:
 		print("Já perdeu")
-	
-func defeat():
-	print("Derrotado")
