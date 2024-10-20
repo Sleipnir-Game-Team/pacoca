51a6c0524471353a6fa54e6d056b6fd941c813cc
FMABr <felipemuamorim@gmail.com>
2024-10-20 13:37:05 -0400

    0.1.17.2 - Coisas

--- entities/Enemy/simple_enemy.gd
+++ entities/Enemy/simple_enemy.gd
@@ -5,7 +5,7 @@ extends CharacterBody2D
 @onready var ball: Ball = get_tree().get_first_node_in_group('Ball')
 
 func _physics_process(_delta: float) -> void:
-	rotation = global_position.direction_to(ball.global_position).angle() - (PI/2)
+	rotation = rotate_toward(rotation, clamp(global_position.angle_to_point(ball.global_position), deg_to_rad(10.0), deg_to_rad(170.0)) - (PI/2), _delta)
 
 func _on_hurt_box_body_entered(body: Node2D) -> void:
 	if body is Ball:
