7efe9eb90a9efcc6a558043d0ac2a91f1bee0c5a
Defala-Jam <defalaemprego@gmail.com>
2024-10-19 21:42:07 -0400

    0.1.12.1[F] - Enemy_inteligence
    inteligencia dos inimigos e tbm adicionei classe ao player para usar ele como referência

--- entities/enemy/enemy_intelligence.gd
+++ entities/enemy/enemy_intelligence.gd
@@ -1,20 +1,33 @@
 extends Node
 
+
 var rng = RandomNumberGenerator.new()
 @export var min_attack_player_chance = 25
 @export var min_trow_chance = 40
 @export var min_trow_to_enemie_chance = 35
+@export var player: Player
+@export var ball: Ball
+@export_group('Kick', 'kick')
 
-var player = preload("res://entities/player/player.tscn")
+#var player = preload("res://entities/player/player.tscn")
+
+@onready var kick = %Kick
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
+
+
+#quando está perto o suficiente ele faz a decisão
+
+
+
+
 func decision():
 	var enemie_decision_number := rng.randi_range(0, 99)
 	
@@ -26,11 +39,32 @@ func decision():
 		trow_ball_to_ally()
 
 func attack_player():
-	var player_position = player.get_global_position()
+	var player_position = player.global_position
+	var inimigo_position = get_parent().global_position
 	
+	
+	kick.trigger(inimigo_position.direction_to(player_position))
+	print("chute dado")
+	
+	
+
 func trow_ball_away():
 	pass
 	
 func trow_ball_to_ally():
 	pass
 	
+
+
+
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print("Área entrou:", area.name)
+	if area.name == "ContactArea":
+		print("encotrou algo")
+		decision()
+
+func _on_kick_hit(direction: Vector2, data: Dictionary) -> void:
+	ball = data.collider as Ball
+	ball.change_angle(rad_to_deg(direction.angle()))
