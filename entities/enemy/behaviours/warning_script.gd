6714a9eb85e6dd983cd7c89355eab2099e6e3c0d
Erik Felipe Viana Rocha <erikviana32@gmail.com>
2024-11-13 12:31:22 -0400

    0.2.5.0 - Finalização do warning

--- entities/enemy/behaviours/warning_script.gd
+++ entities/enemy/behaviours/warning_script.gd
@@ -1,14 +1,14 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+@onready var warning_sprite = $warning_sprite as Sprite2D
+@onready var warning_timer = $warning_timer as Timer
+@onready var warning_animation = $warning_animation as AnimationPlayer
 
 func warning(position: Vector2):
-	return
+	warning_sprite.global_position = position
+	warning_animation.play("Warning")
+	warning_timer.start()
+
+func _on_warning_timer_timeout() -> void:
+	warning_animation.stop()
+	warning_sprite.visible = false
